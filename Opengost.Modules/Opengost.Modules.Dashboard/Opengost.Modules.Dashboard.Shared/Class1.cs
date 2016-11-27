6c75c0da0919ecc2cf2c202c92f81f37e2b035ab
Aleksander Bešir <alex.besir@gmail.com>
2016-11-27 13:36:45 +0100

    Initial opengost structure

---
+++ Opengost.Modules/Opengost.Modules.Dashboard/Opengost.Modules.Dashboard.Shared/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opengost.Modules.Dashboard.Shared
+{
+    public class Class1
+    {
+    }
+}
